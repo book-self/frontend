d4f6619ec29eade5d962938f81238ca7f66ec279
David Engel <david@airshipdevelopment.com>
2021-04-27 22:56:41 -0700

    Adds Redux and example component
    - Redux was installed via Redux Toolkit: https://redux-toolkit.js.org
    - Example component is only meant to show proof of concept

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Counter } from './components/counter/Counter';
 
 function App() {
   return (
@@ -17,6 +18,18 @@ function App() {
         >
           Learn React
         </a>
+        
+        <Counter />
+        <p>
+          <a
+          className="App-link"
+          href="https://redux.js.org/introduction/getting-started"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+            Learn Redux
+          </a>
+        </p>
       </header>
     </div>
   );
