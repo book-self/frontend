e5114b5435b20c7bd829703248334bc6af92246e
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-04 15:17:34 -0500

    Add in a new reducer for the display of a single book

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './components/counter/counterSlice';
+import singelbookdisplayReducer from './components/bookList/singleBookDisplay/singleBookDisplaySlice'
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    singlebookdisplay: singelbookdisplayReducer,
+
   },
 });
