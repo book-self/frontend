7d24c9fb64bc6ffd32aba67ddba5e957b897fd53
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-06 10:46:33 -0500

    Switch from a single book display to book list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {SingleBookDisplay} from "./components/bookList/singleBookDisplay/SingleBookDisplay"
+import {BookList} from "./components/bookList/bookListDisplay/BookList"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SingleBookDisplay/>
+        <BookList/>
       </header>
 
     </div>
