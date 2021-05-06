6e61f4402eca4d69cdfa01c4bf2a3051dbeecb0c
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-06 11:16:38 -0500

    Import in the single book display so that the temporary data can be rendered.

--- src/components/bookList/bookListDisplay/BookList.js
+++ src/components/bookList/bookListDisplay/BookList.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
+import {shallowEqual } from 'react-redux'
+
 import { useSelector, useDispatch } from 'react-redux';
 
+import {SingleBookDisplay} from "../singleBookDisplay/SingleBookDisplay"
+
+const selectBookList = (state) => state.booklistdisplay.map((book) => book.bookID)
 
 export function BookList(){
+    const booksTempList = useSelector(selectBookList, shallowEqual)
 
+
+    const renderedListItems = booksTempList.map((book) => {
+        return <SingleBookDisplay key={book} id={book} />})
+
+    return(
+        <div><ul className="book-list">{renderedListItems}</ul></div>
+    )
 }
