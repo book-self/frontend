9a1e7ac7420b429bf09aef22a6694374704e94eb
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-07 23:47:16 -0500

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import {SingleBookDisplay} from "./components/bookList/singleBookDisplay/SingleBookDisplay"
 import {BookList} from "./components/bookList/bookListDisplay/BookList"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BookList/>
+        <BookList />
       </header>
 
     </div>
