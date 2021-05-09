7117581c463ab594403fa78167d24af35e24785e
David Engel <davidengel@live.com>
2021-05-09 15:29:20 -0700

    Use arrow functions in components (#10)
    * Add basic routing
    
    * Add basic tests to routes. Remove background.
    
    * Update declarations
    
    Example: export const example = () => {}
    
    * Fix tests

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import { Switch, Route } from "react-router-dom";
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Profile from './pages/profile/Profile'
-import NotFound from './pages/notFound/NotFound'
+import { Home } from './pages/home/Home'
+import { Login } from './pages/login/Login'
+import { Profile } from './pages/profile/Profile'
+import { NotFound } from './pages/notFound/NotFound'
 
-export function Router() {
+export const Router = () => {
   const routes = [
     {
       path: "/",
