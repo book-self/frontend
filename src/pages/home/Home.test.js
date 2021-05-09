fc3d7d9794e50e5e2792ee16f2ea4c32f0ae874a
David Engel <davidengel@live.com>
2021-05-09 12:40:48 -0700

    Add basic tests to routes. Remove background.

---
+++ src/pages/home/Home.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+test('renders learn react link', () => {
+  render(
+    <Home/>
+  );
+
+  const link = screen.getByText(/learn react/i);
+  expect(link).toBeInTheDocument();
+});
