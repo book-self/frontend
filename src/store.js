9a3c8bec2f135505b16e10ceaf403a03fabe45d1
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-09 21:52:10 -0500

    Sorce of conflict

--- src/store.js
+++ src/store.js
@@ -1,13 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-
-import counterReducer from './components/counter/counterSlice';
-import booklistReducer from './components/bookList/bookListDisplay/bookListSlice'
+import counterReducer from './components/Counter/counterSlice';
 
 export const store = configureStore({
-  
-    reducer:{
-        counter: counterReducer,
-        booklistdisplay: booklistReducer
-    }
+  reducer: {
+    counter: counterReducer,
+  },
 });
-
