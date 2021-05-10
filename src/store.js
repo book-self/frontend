92b3b6fcb1ea57819eca6727608ecd157af4adb3
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-09 21:42:15 -0500

    Fixed things up

--- src/store.js
+++ src/store.js
@@ -1,6 +1,14 @@
 import { applyMiddleware, combineReducers, configureStore, createStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers'
 
+import counterReducer from './components/counter/counterSlice';
+import booklistReducer from './components/bookList/bookListDisplay/bookListSlice'
+
+export const store = combineReducers({
+  
+    counter: counterReducer,
+    booklistdisplay: booklistReducer
 
 
-export const store = createStore(rootReducer);
+});
+
