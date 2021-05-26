5c8df82751c7bc09b0a14947d1a10ef56f268ec9
Christian Kleinvehn <ckleinvehn@gmail.com>
2021-05-26 07:40:34 -0500

    Forgot to add these.

---
+++ src/pages/Book/UserRatingStyles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+    review: {
+        width: "50%",
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'flex-start'
+    },
+
+    textField: {
+        padding: "30px"
+    }
+});
