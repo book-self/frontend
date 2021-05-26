fa9fa584d5f717e58f6fbaff4e0bdc5fa28c71a4
nardost <nardos.tessema@gmail.com>
2021-05-25 23:07:53 -0500

    Reinstate the book list grid view.

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,15 +1,10 @@
 import { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 
-// import { Container } from '@material-ui/core';
-// import Shelf from '../../components/Shelf/Shelf';
-// import { fetchBookLists } from '../../components/Shelf/ShelfFetch';
 import { Link } from "react-router-dom";
 import {fetchUserBookLists} from './ProfileFetch';
 export const Profile = () => {
 
-  //const [userId = 8;
-  //const [userId, setUserId] = useState(null);
   const userId = null;
   const [bookLists, setBookLists] = useState([]);
 
@@ -23,29 +18,7 @@ export const Profile = () => {
     getUserBookLists();
   },[userId]
     )
-  /**
-   * Component Shelf lists all the book lists of a user.
-   * Commented out here until login feature is completed and
-   * we know where to get user id from...
-   * Login guys: where do we get user id from?
-  const userId = 8;
-  const [bookLists, setBookLists] = useState([]);
 
-  useEffect(() => {
-    (
-      async () => {
-        const { data } = await fetchUserBookLists(userId);
-        setBookLists(data);
-      }
-    )()
-  }, [])
-
-  return (
-    <Container maxWidth="sm">
-      <Shelf lists={ bookLists } />
-    </Container>
-  );
-   */
   return(
     <div>
       Profile
