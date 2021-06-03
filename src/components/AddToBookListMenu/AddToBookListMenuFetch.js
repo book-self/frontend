85cd58f81e8eae90e10afb40f6bba077707399b8
Christian Kleinvehn <ckleinvehn@gmail.com>
2021-06-03 17:55:15 -0500

    Componentizing.

--- src/components/AddToBookListMenu/AddToBookListMenuFetch.js
+++ src/components/AddToBookListMenu/AddToBookListMenuFetch.js
@@ -12,3 +12,17 @@ export function postBooksToList(updatedListName, booksToAdd, idOfNewList, idOfOl
   return fetch(`${process.env.REACT_APP_API_URL}/v1/book-lists/${idOfOldList}/update`, requestOptions)
   .then(response =>response.json());
 }
+
+    
+//  const requestOptions = {
+//    method: 'PUT',
+//    crossDomain:true,
+//    mode: 'cors',
+//    headers: {
+//    'Content-Type':'application/json'},
+//    body: JSON.stringify({ newListName: updatedListName, newBookListId: idOfNewList, booksToBeAdded: booksToAdd, booksToBeRemoved: []})
+//  }
+//  console.log(requestOptions);
+//  return fetch(`${process.env.REACT_APP_API_URL}/v1/book-lists/${idOfOldList}/update`, requestOptions)
+//  .then(response =>response.json());
+//  }
